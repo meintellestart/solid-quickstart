ce268c69e9c5f08fe6387634688ebc1555ea0458
meintellestart <93955406+meintellestart@users.noreply.github.com>
2023-08-16 17:35:03 -0600

    thirdtry

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,16 +5,18 @@ export default function Home() {
   return (
     <main>
       <Title>Hello Mars</Title>
-      <h1>Kristopher Pearson has been appointed to your case in Juvenile Court</h1>
-      <h2>In order to begin your case, your attorney needs inofrmation from you</h2>
-      <p>If you have been charged with a crime (if you were an adult), then you must complete the Juvenile Delinquency Form</p>
-      <p>
-        Visit{" "}
-        <a href="https://forms.gle/KNobhrvUtLhUn57TA" target="_blank">
+      <h1>Saint George Defender</h1>
+      <h2>You were appointed Kristopher Pearson as your lawyer in Juvenile Court.</h2>
+      <p>In order to begin your case, your attorney needs inofrmation from you</p>
+      <p>If you have been charged with a crime (if you were an adult), then you must 
+        complete the form below.</p>
+      <p> 
+        Visit {" "}
+       <a href="https://forms.gle/KNobhrvUtLhUn57TA" target="_blank">
           Juvenile Delinquency Form
         </a>{" "}
-        to complete the form.
-      </p>
+      </p> 
+
       <Counter />
  
     </main>
